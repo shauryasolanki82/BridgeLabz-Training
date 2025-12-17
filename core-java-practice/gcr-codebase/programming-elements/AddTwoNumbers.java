788b6b72d2d1c19b1bdf5ddf4372bb94369e90c1
shauryasolanki82 <chshauryasolanki@gmail.com>
2025-12-17 16:00:49 +0530

    first program

---
+++ core-java-practice/gcr-codebase/programming-elements/AddTwoNumbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
